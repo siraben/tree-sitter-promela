8fe2aacb00d51d8738b098449a8710592c1d74aa
Ben Siraphob <bensiraphob@gmail.com>
2022-02-10 23:41:26 -0600

    Add query highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,44 @@
+(comment) @comment
+
+[
+ "active"
+ "assert"
+ "atomic"
+ "break"
+ "d_step"
+ ;; "D_proctype"
+ "do"
+ "else"
+ ;; "empty"
+ "enabled"
+ "fi"
+ ;; "full"
+ ;; "goto"
+ "hidden"
+ "if"
+ "init"
+ "len"
+ ;; "nempty"
+ "never"
+ ;; "nfull"
+ "od"
+ "of"
+ "pc_value"
+ "printf"
+ "priority"
+ "proctype"
+ "provided"
+ "run"
+ (skip)
+ "timeout"
+ "typedef"
+ "unless"
+ ;; "unsigned"
+ "xr"
+ "xs"
+ ] @keyword
+
+
+["bit" "bool" "byte" "chan" "int" "mtype" "pid" "short"] @type
+
+((uname) @variable.builtin)
